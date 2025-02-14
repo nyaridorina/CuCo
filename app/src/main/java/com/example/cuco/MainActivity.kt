51a2cb1d1eaff4d32543b23646a474e37de44149
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2025-02-14 21:42:40 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/cuco/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.cuco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.camera.lifecycle.ProcessCameraProvider
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var resultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.result_text)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val analyzer = CameraAnalyzer { detectedPrice ->
+                CurrencyConverter.convertCurrency(detectedPrice) { convertedValue ->
+                    runOnUiThread { resultTextView.text = "Converted: $convertedValue" }
+                }
+            }
+            cameraProvider.bindToLifecycle(this, analyzer)
+        }, cameraExecutor)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+}
